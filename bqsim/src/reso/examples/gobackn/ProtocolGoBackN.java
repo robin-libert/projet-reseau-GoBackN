cb04fc6a1486f884dc2b15485e8af4325d33e73b
PC-Robin <xenoeus@gmail.com>
2018-04-08 11:40:09 +0200

    ajout classe ProtocolGoBackN

--- bqsim/src/reso/examples/gobackn/ProtocolGoBackN.java
+++ bqsim/src/reso/examples/gobackn/ProtocolGoBackN.java
@@ -1,3 +1,15 @@
-public class ProtocolGoBackN{
+package reso.examples.gobackn;
+
+
+import reso.ip.Datagram;
+import reso.ip.IPInterfaceAdapter;
+import reso.ip.IPInterfaceListener;
+
+public class ProtocolGoBackN implements IPInterfaceListener{
+
+    @Override
+    public void receive(IPInterfaceAdapter src, Datagram datagram) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 	
 }
